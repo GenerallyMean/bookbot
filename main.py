288658194e31987e847e3975265fd0b715d7c81b
Jason <generallymean@gmail.com>
2024-07-25 01:20:17 -0500

    report added, book recall removed

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    num_text = get_num_text(text)
+    chars_sorted_list = chars_dict_to_sorted_list(num_text)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in this document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End Report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -11,6 +23,27 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def chars_dict_to_sorted_list(num_chars_dict):
+    def sort_on(dict_item):
+        return dict_item["num"]
+
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True,key=sort_on)
+    return sorted_list
+
+    
+def get_num_text(text):
+    letters = {}
+    for l in text:
+        lowered_text = l.lower()
+        if lowered_text in letters:
+            letters[lowered_text] += 1
+        else:
+            letters[lowered_text] = 1
+    return letters
 
 
 
